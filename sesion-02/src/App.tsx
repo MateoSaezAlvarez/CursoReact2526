701984f9f13361fb3e50e5f8b2888d4de45065c4
MateoSaezAlvarez <msaezalvarez735@gmail.com>
2026-01-14 14:41:33 +0100

    Create sesion 02 React

---
+++ sesion-02/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-slate-50 font-sans'>
+      <Header />
+      <main>
+        {/* Aqui vendrán dos componentes */}
+      </main>
+    </div>
+  )
+}
+
+export default App
